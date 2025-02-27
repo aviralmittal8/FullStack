56315728775f9cccdfee101d4995bbf14568aef3
Aviral <techieaviral11@gmail.com>
2025-02-27 12:47:50 +0530

    loggin backend

--- Backend/httpModule/NodeRegistration/app.js
+++ Backend/httpModule/NodeRegistration/app.js
@@ -1,33 +1,30 @@
-const http = require("http");
-const fs = require("fs").promises;
-
-const PORT = 3001;
-const server = http.createServer((req, res) => {
-  // res.end("Welcome to node server")
-  const { url, method } = req;
-  if (url === "/register" && method === "POST") {
-    let body = "";
-    let arr = [];
-    req.on("data", (chunck) => {
-      body += chunck;
-    });
-    req.on("end", async () => {
-      const { name, email, password } = JSON.parse(body);
-      console.log(name);
-      await fs.writeFile(
-        "student.json",
-        JSON.stringify({ name, email, password })
-      );
-      // console.log(body);
-    });
-    res.end(
-      JSON.stringify({
-        message: "/register api hit successfully",
-      })
-    );
-  }
-});
-
-server.listen(PORT, () => {
-  console.log("Server listen on " + PORT);
-});
+const http= require('http')
+const fs=require('fs').promises
+const PORT= 3001
+const server= http.createServer((req,res)=>{
+    if(req.url=="/register" && req.method=="POST"){
+        let body=''
+        let arr=[]
+        req.on('data',chunk=>{
+            
+            body+=chunk;
+        })
+        req.on('end', async()=>{
+            const {email,password}=JSON.parse(body)
+            const fdata= await fs.readFile('student.json',{encoding:'utf-8'})
+            arr=JSON.parse(fdata)
+            // console.log(arr)
+            const result=arr.find(ele=>ele.email==email && ele.password == password)
+            if(result){
+                //res.setHeader('Content-Type','application/json')
+                return res.end(JSON.stringify({"message":"Logged in"}))
+            }
+            arr.push({email,password})
+            await fs.writeFile('student.json',JSON.stringify(arr,null,2))
+            res.end(JSON.stringify({"message":"/register api hit successfully!"}))
+        })
+    }
+})
+server.listen(PORT,()=>{
+    console.log("server is running on " + PORT)
+})
